183ff0a9928c8d2e5711ea81a3d69c004225e512
ivanovich <vania200345@gmail.com>
2025-03-19 12:28:13 +0200

    Add shared preferences

---
+++ app/src/main/java/com/nahadayka/model/SharedPreferencesRepository.kt
@@ -0,0 +1,37 @@
+package com.nahadayka.model
+
+import android.content.Context
+import androidx.core.content.edit
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class SharedPreferencesRepository @Inject constructor(
+    @ApplicationContext private val context : Context
+) {
+    private val sharedPreferences = context.getSharedPreferences("nahadayka", Context.MODE_PRIVATE)
+
+
+    fun getString(key : String): String {
+        val data = sharedPreferences.getString(key, "")
+        data?.let{
+            return it
+        }
+        return ""
+    }
+
+    fun saveString(value : String, key : String){
+        sharedPreferences.edit() { putString(key, value) }
+    }
+
+    fun removeString(key : String){
+        sharedPreferences.edit() { remove(key) }
+    }
+
+
+
+
+
+
+
+
+}
